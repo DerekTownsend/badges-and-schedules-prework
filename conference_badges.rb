c48cf20b8f4ce3155038206cf6e30f577c21c70b
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 03:25:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,10 @@
 # Write your code here.
 def batch_badge_creator(array)
+  new_list=[]
   array.each do |name|
     puts "Hello, my name is #{name}."
   end
+  return new_list
 end
 
 def assign_rooms(array)
