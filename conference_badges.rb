8d1ae7df4462e3a5f8456101860f19b23228897e
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 03:33:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 # Write your code here.
 def badge_maker(name)
   output="Hello, my name is #{name}."
-  puts output
   return output
 end
 def batch_badge_creator(array)
@@ -15,7 +14,7 @@ end
 def assign_rooms(array)
   roomlist=[]
   array.each do |name|
-    new_list.push(badge_maker(name))
+    roomlist.push(badge_maker(name))
   end
-  return new_list
+  return roomlist
 end
